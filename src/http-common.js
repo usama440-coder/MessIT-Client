8ece10bdd4a1c15ddac02c803216ec5ec6557d33
Usama Shaukat <usamamian440@gmail.com>
2023-05-26 01:41:34 +0500

    changing base url

--- src/http-common.js
+++ src/http-common.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-  // baseURL: "https://brainy-teal-nightgown.cyclic.app/api/v1",
-  baseURL: "http://localhost:5000/api/v1",
+  baseURL: "https://brainy-teal-nightgown.cyclic.app/api/v1",
+  // baseURL: "http://localhost:5000/api/v1",
 });
